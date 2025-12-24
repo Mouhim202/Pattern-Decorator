c0107785eb9ecbdec226b05fc829d37917d35121
unknown <meryemmouhim123@gmail.com>
2025-12-25 00:38:15 +0100

    V3

---
+++ README.md
@@ -0,0 +1,3 @@
+<img src="Captures/1.png">
+<br><br>
+<img src="Captures/2.png">
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ import composants.Espresso;
 import composants.Sumatra;
 import decorateurs.Caramel;
 import decorateurs.Chocolat;
+import decorateurs.Noisette;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,19 +12,23 @@ import java.io.InputStreamReader;
 
 public class Test {
     public static void main(String[] args) {
-        Boisson boisson;
-        boisson=new Sumatra();
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
-        System.out.println("--------------------");
-        boisson=new Chocolat(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
-        System.out.println("-------------------------");
-        boisson=new Caramel(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
+//        Boisson boisson;
+//        boisson=new Sumatra();
+//        System.out.println(boisson.getDescription());
+//        System.out.println(boisson.cout());
+//        System.out.println("--------------------");
+//        boisson=new Chocolat(boisson);
+//        System.out.println(boisson.getDescription());
+//        System.out.println(boisson.cout());
+//        System.out.println("-------------------------");
+//        boisson=new Caramel(boisson);
+//        System.out.println(boisson.getDescription());
+//        System.out.println(boisson.cout());
 
+        Boisson b;
+        b = new Chocolat(new Caramel(new Chocolat(new Caramel(new Noisette(new Sumatra())))));
+        System.out.println(b.getDescription());
+        System.out.println(b.cout());
 
     }
 }
